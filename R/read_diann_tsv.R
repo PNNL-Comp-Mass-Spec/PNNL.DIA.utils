876bd6e5278324a77aabdf5884a865162f7d94e4
DavidRossHall <david.ross.hall@protonmail.com>
2023-12-28 15:19:45 -0800

    renamed first function

--- R/read_diann_tsv.R
+++ R/read_diann_tsv.R
@@ -1,15 +1,15 @@
 
-#' Reads DIA-NN Precursor report
+#' Reads DIA-NN `.tsv` output files
 #'
-#' @param path Filepath to DIA-NN `report.tsv`
+#' @param path Filepath to DIA-NN `.tsv` output file 
 #'
 #' @return A data frame 
 #' @export
 #'
 #' @examples
 #' x <- system.file("extdata", "diann_report.tsv", package = "pnnl.diann.utils")
-#' read_pq_report(x)
-read_pq_report <- function(path){
+#' read_diann_tsv(x)
+read_diann_tsv <- function(path){
   
   df <- data.table::fread(path) |>
     as.data.frame()
