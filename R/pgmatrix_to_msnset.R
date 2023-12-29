ad80bd20a06f7f8bbe3de2f469258a218844d7b9
DavidRossHall <david.ross.hall@protonmail.com>
2023-12-28 17:13:16 -0800

    addded generic function to read matrix

---
+++ R/pgmatrix_to_msnset.R
@@ -0,0 +1,60 @@
+
+
+#' Converts DIA-NN matrix report to msnset object
+#'
+#' @param path Filepath to DIA-NN protein or gene group output file
+#' @param fdata_rownames Which matrix column to be used in fData slot rowname
+#'
+#' @return MSnSet object 
+#' @export
+#'
+#' @examples
+#' 
+#' x <- system.file("extdata",
+#'   "QC_Mam_19_01_b_DIA_report.pg_matrix.tsv", 
+#'    package = "pnnl.diann.utils")
+#'    
+#' pgmatrix_to_msnset(x)
+pgmatrix_to_msnset <- function(path, fdata_rownames = "Protein.Group"){
+  
+  df <- read_diann_tsv(path)
+  
+  # f_data 
+  
+  f_data_cols <- c("Protein.Group",
+                   "Protein.Ids",
+                   "Protein.Names",
+                   "Genes",
+                   "First.Protein.Description")
+  
+  f_data <- df |>
+    dplyr::select(dplyr::any_of(f_data_cols)) |>
+    tibble::column_to_rownames(var = fdata_rownames)
+  
+  # exprs data 
+  
+  exprs <- df |>
+    dplyr::select(-dplyr::any_of(f_data_cols)) |>
+    dplyr::rename_with(
+      ~ stringr::str_remove_all(
+        basename(.x), "\\..*")
+      ) |>
+    as.matrix()
+    
+  # p_data
+  
+  p_data <- data.frame(dataset = colnames(exprs)) |>
+    `rownames<-`(colnames(exprs))
+    
+  
+  # assemble msnset
+  
+  m <- MSnbase::MSnSet(
+    exprs = exprs, 
+    fData = f_data,
+    pData = p_data
+  )
+  
+  m
+
+}
