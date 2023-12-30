a9ddabcebe972c7efc014982b1894e664e117244
DavidRossHall <david.ross.hall@protonmail.com>
2023-12-29 19:04:31 -0800

    need to finish pqreport_to_msnset()

--- R/pqreport_to_msnset.R
+++ R/pqreport_to_msnset.R
@@ -1,20 +1,39 @@
 
-# 
-# pqreport_to_msnset <- function(x, 
-#                                id_header = "Precursor.Id", 
-#                                quantity_header = "Precursor.Normalised",
-#                                proteotypic_only = F, 
-#                                q = 0.01,
-#                                protein_q = 1.0,
-#                                pg_q = 1.0, 
-#                                gg_q = 1.0) {
-#   
-#   df <- as.data.table(x)
-#   
-#   if(proteotypic_only){
-#     df <- df |>
-#       filter(Protetypic != )
-#   }
-#   
-# }
-#   
+
+pqreport_to_msnset <- function(x,
+                               id_header = "Precursor.Id",
+                               quantity_header = "Precursor.Normalised",
+                               proteotypic_only = F,
+                               q = 0.01,
+                               protein_q = 1.0,
+                               pg_q = 1.0,
+                               gg_q = 1.0) {
+
+  df <- as.data.frame(x)
+
+  # Filtering df based on provided cut-offs
+
+  if(proteotypic_only){
+    df <- df |>
+      dplyr::filter(Protetypic != 0)
+  }
+
+  df <- df |>
+    dplyr::filter({{id_header}} != "") |>
+    dplyr::filter({{quantity_header}} > 0) |>
+    dplyr::filter("Q.Value" < q) |>
+    dplyr::filter("Protein.Q.Value" < protein_q) |>
+    dplyr::filter("PG.Q.Value" < pg_q) |>
+    dplyr::filter("GG.Q.Value" < gg_q)
+
+  # checking for duplicate precursors in ind. runs.
+  is_duplicated = any(duplicated(paste0(df[["File.Name"]],":",df[[id_header]])))
+
+  if (is_duplicated){
+    warning("Multiple quantities per id: the maximum of these will be calculated")
+
+  }
+
+  
+  
+}
