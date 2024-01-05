a81fcf8674cdb9d7fdedb278773e9de9df7887af
DavidRossHall <david.ross.hall@protonmail.com>
2024-01-05 15:41:32 -0800

    maxlfq works, but need to output msnset

--- R/msnset_maxLFQ.R
+++ R/msnset_maxLFQ.R
@@ -1,14 +1,32 @@
-x_path <- system.file("extdata",
-                      "diann_report.tsv",
-                      package = "pnnl.diann.utils")
-
-x <- read_diann_tsv(x_path)
 
 
-View(x)
 
+#' MaxLFQ quantites of precursors in msnset
+#'
+#' @param m DIA-NN precursors report as MSnSet from `pqreport_to_msnset()`
+#'
+#' @return list of maxLFQ intensities
+#' @export
+#'
+#' 
+msnset_maxLFQ <- function(m){
+  
+  ls <- MSnbase::exprs(m) |>
+    as.data.frame() |>
+    tibble::rownames_to_column(var= "id") |>
+    cbind(MSnbase::fData(m)[, "Protein.Group"]) |>
+    dplyr::rename("protein_list" = `MSnbase::fData(m)[, "Protein.Group"]`) |>
+    dplyr::filter(protein_list != "") |>
+    tidyr::pivot_longer(cols = -c(protein_list, id), 
+                        names_to = "sample_list", 
+                        values_to = "quant") |>
+    dplyr::filter(!is.na(quant)) |>
+    dplyr::mutate(quant = log2(quant)) |>
+    as.list()
+  
+  ls_maxlfq <- iq::fast_MaxLFQ(ls)
+  
+  ls
+  
+}
 
-iq::process_long_format(x_path, output_filename = "report-pg-annotated.tsv",
-                        annotation_col = c("Protein.Names", "Genes"))
-
-fun <- utils::getFromNamespace("fast_MaxLFQ", "iq")
