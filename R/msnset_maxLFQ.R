bbbbf0aa1eabce8e050f65636f6ae9392b94765b
DavidRossHall <david.ross.hall@protonmail.com>
2024-01-08 16:15:59 -0800

    log2_trans option

--- R/msnset_maxLFQ.R
+++ R/msnset_maxLFQ.R
@@ -3,23 +3,24 @@
 
 #' MaxLFQ quantites of precursors in msnset
 #'
-#' @param m DIA-NN precursors report as MSnSet from `pqreport_to_msnset()`
+#' @param m DIA-NN precursors report as MSnSet from `pqreport_to_msnset()`; should be **log2 intensities**
 #' @param level Level to perform MaxLFQ, either "Protein.Group" or "Genes"
+#' @param log2_trans Default TRUE; pq intensities need to be in Log2 space for `iq::fast_maxlfq()`
 #' @return list of maxLFQ intensities
 #' @export
 #'
 #' 
-msnset_maxLFQ <- function(m, level = c("Protein.Group", "Genes")){
+msnset_maxLFQ <- function(m, level = c("Protein.Group", "Genes"), log2_trans = TRUE){
   # To DO 
   # check for missing protein group info (could be general msnset function)
   # pipeline for msnset normalization 
   
   # to pass R CMD CHECK
-  
-  `MSnbase::fData(m)[, level]` <- `protein_list` <- `id` <- `quant` <- File.Name <- NULL 
+    `MSnbase::fData(m)[, level]` <- `protein_list` <- `id` <- `quant` <- File.Name <- NULL 
   
   level <- match.arg(level)
   
+  # Reshaping exprs data to work with iq::fast_maxlfq()
   ls <- MSnbase::exprs(m) |>
     as.data.frame() |>
     tibble::rownames_to_column(var= "id") |>
@@ -29,8 +30,15 @@ msnset_maxLFQ <- function(m, level = c("Protein.Group", "Genes")){
     tidyr::pivot_longer(cols = -c(protein_list, id), 
                         names_to = "sample_list", 
                         values_to = "quant") |>
-    dplyr::filter(!is.na(quant)) |>
-    dplyr::mutate(quant = log2(quant)) |>
+    dplyr::filter(!is.na(quant)) 
+  
+  # applying log 2 trans if TRUE
+  if(log2_trans == TRUE){
+    ls <- ls |>
+      dplyr::mutate(quant = log2(quant))
+  }
+    
+  ls <- ls |>
     as.list()
   
   
