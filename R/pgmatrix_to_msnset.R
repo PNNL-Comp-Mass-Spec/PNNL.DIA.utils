0dabfd8ba21ff1889132da5b1713a4b7d99d1af8
Vlad Petyuk <petyuk@gmai.com>
2026-04-13 11:45:49 -0700

    more robust handling of split into feature and sample name columns

--- R/pgmatrix_to_msnset.R
+++ R/pgmatrix_to_msnset.R
@@ -3,6 +3,9 @@
 #' Converts DIA-NN matrix report to msnset object
 #'
 #' @param x protein groups matrix read  into R
+#' @param sample_tag Either a substring matching sample names, 
+#' can be a vector of multiple substrings or vector of sample names.
+#' For now the default is "raw" meaning that the datasets are Thermo raw files.
 #' @param id_header Which matrix column to be used in fData slot rowname
 #'
 #' @importFrom dplyr select any_of rename_with
@@ -23,17 +26,19 @@
 #' pg <- read_diann_tsv(pg_path) 
 #'    
 #' pgmatrix_to_msnset(pg)
-pgmatrix_to_msnset <- function(x, id_header = "Protein.Group"){
+pgmatrix_to_msnset <- function(x, sample_tag = "raw", id_header = "Protein.Group"){
   
   df <- as.data.frame(x)
   
-  # f_data 
+  sample_idx <- grepl(paste(sample_tag, collapse = "|"), colnames(df))
   
-  f_data_cols <- c("Protein.Group",
-                   "Protein.Ids",
-                   "Protein.Names",
-                   "Genes",
-                   "First.Protein.Description")
+  # f_data 
+  # f_data_cols <- c("Protein.Group",
+  #                  "Protein.Ids",
+  #                  "Protein.Names",
+  #                  "Genes",
+  #                  "First.Protein.Description")
+  f_data_cols <- colnames(df)[!sample_idx]
   
   f_data <- df |>
     dplyr::select(dplyr::any_of(f_data_cols)) |>
